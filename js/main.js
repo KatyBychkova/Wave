cefa4c586da6bf415b04a44bbeb64c349615d94f
Katya_Fedorovka <kats@list.ru>
2021-08-03 22:56:24 +0300

    slider-blog complite

--- js/main.js
+++ js/main.js
@@ -17,31 +17,16 @@ $(function(){
 
   $('.slider-blog__inner').slick({
     dots : true,
-    prevArrow : '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
-    nextArrow : '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>'
+    arrows : false
+   
   });
 
-  // $('.gallery__content').slice(0, 6).show()
-  // $('.gallery__more-link').on('click', function(){
-  //   $('.gallery__content:hidden').slice(0, 6).slideDown()
-  //   // if($('.gallery__content-list:hidden').length = 0) {
-  //   //   $('.gallery__more-btn').fadeOut('slow')
-  //   // }
-  // })
-
+  
  
 });
 
 
 
-// $(function(){
-
-//   $('div').slice(0, 4).show()
-//  $('.load__more').on('click', function(){
-//    $('div:hidden').slice(0, 4).slideDown()
-//    })
-  
-// })
 
 
 
